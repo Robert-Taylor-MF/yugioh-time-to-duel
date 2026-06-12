75449ee78383e6a214ec3097f56ab50222c13303
Robert Taylor de M. Ferreira <56204130+Robert-Taylor-MF@users.noreply.github.com>
2026-06-12 11:34:39 -0300

    feat: adicionar capacitor e github actions apk builder

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yugioh.timetoduel',
+  appName: 'yugioh-time-to-duel',
+  webDir: 'dist'
+};
+
+export default config;
